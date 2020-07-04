81c408e88de677771cfa540c1bc9ebce6d36882a
Karbovskiy Dmitriy <karbovskiydmitriy99@gmail.com>
2020-07-04 19:24:31 +0300

    Update Template.h

--- OpenGLTemplate/OpenGLTemplate/Template.h
+++ OpenGLTemplate/OpenGLTemplate/Template.h
@@ -5,8 +5,8 @@
 #pragma comment(lib, "opengl32.lib")
 #pragma comment(lib, "glu32.lib")
 
-#define CLASS_NAME "Smoothing test"
-#define WS_CUSTOM WS_VISIBLE | WS_POPUP | WS_MAXIMIZE
+#define CLASS_NAME "OpenGLTemplateClass"
+#define WS_CUSTOM (WS_VISIBLE | WS_POPUP | WS_MAXIMIZE)
 #define COLOR_DEPTH 24
 
 void main();
